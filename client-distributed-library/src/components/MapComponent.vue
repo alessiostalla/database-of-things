09126a20ad2f6de9520e3b29485330ca6e1ad646
Alessio Stalla <alessiostalla@gmail.com>
2023-01-27 07:26:21 +0100

    Get the user's location from the browser

--- client-distributed-library/src/components/MapComponent.vue
+++ client-distributed-library/src/components/MapComponent.vue
@@ -2,13 +2,25 @@
 import "leaflet"; //See https://github.com/vue-leaflet/vue-leaflet/issues/251#issuecomment-1356978564
 import { LMap, LTileLayer, LControlLayers } from "@vue-leaflet/vue-leaflet";
 import "leaflet/dist/leaflet.css";
-import {ref} from "vue";
+import {onBeforeMount, ref} from "vue";
+import VueGeolocation from "vue3-geolocation";
 
 defineProps<{}>()
 const zoom = ref(17);
 const log = (it: any) => {
   console.log(it);
 };
+const location = ref({
+  lat: 44.34850, lng: 9.23400
+});
+onBeforeMount(async () => {
+  VueGeolocation.getLocation().then((coordinates: any) => {
+    location.value = coordinates;
+    console.log("User's location", coordinates);
+  }).catch((error: any) => {
+    console.log(error);
+  });
+});
 </script>
 
 <template>
@@ -16,7 +28,7 @@ const log = (it: any) => {
     <l-map
         v-model="zoom"
         v-model:zoom="zoom"
-        :center="[44.34850, 9.23400]"
+        :center="[location.lat, location.lng]"
         @move="log('move')"
     >
       <l-tile-layer
--- client-distributed-library/src/main.ts
+++ client-distributed-library/src/main.ts
@@ -7,17 +7,16 @@ import router from './router'
 
 import './assets/main.css'
 import {messages} from "@/messages";
+import VueGeolocation from "vue3-geolocation";
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueGeolocation);
 app.use(createI18n({
     locale: 'it',
     fallbackLocale: 'en',
     messages,
 }));
-
-
-
 app.mount('#app')
